b12ba03ba852e1384e118fc8a35fc3c0f78f2130
pepsvazcor <vaz.cor@hotmail.com>
2024-05-15 08:23:13 -0600

    cambios punto 5

---
+++ LeaveManagement.Web/Data/Migrations/20240514194956_AddedLeave.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLeave : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LeaveTypeId",
+                table: "LeaveTypes",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfDays",
+                table: "LeaveAllocations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfDays",
+                table: "LeaveAllocations");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "LeaveTypes",
+                newName: "LeaveTypeId");
+        }
+    }
+}
---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagement.Web.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        [Display (Name = "Default number of days")]
+        [Required]
+        [Range(1,25,ErrorMessage ="pleaseenter a valid number")]
+        public int DefaultDays{ get; set; }
+    }
+}
